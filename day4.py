a16b03904f1f0b1de68564618f4c55ae5a6cbf1b
Lasse Hinrichsen-Bischoff <lh1887@mi.fu-berlin.de>
2024-12-04 09:35:32 +0100

    Day 4 setup

---
+++ day4.py
@@ -0,0 +1,9 @@
+def parse_input(file_name) -> list[list[int]]:
+    pass
+
+
+if __name__ == "__main__":
+    print(parse_input("day4.txt"))
+    # input = parse_input("day4.txt")
+    # print(part_1(input))
+    # print(part_2(input))
