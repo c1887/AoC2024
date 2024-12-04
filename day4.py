3aa792fb934acca9eef0c3b9eb6cc572c7bada78
Lasse Hinrichsen-Bischoff <lh1887@mi.fu-berlin.de>
2024-12-04 10:37:47 +0100

    half working day 4 part 1

--- day4.py
+++ day4.py
@@ -38,11 +38,59 @@ def down(grid: list[str], row: int, col: int) -> bool:
     word = list_to_single_str([grid[i][col] for i in range(row + 1, row + 4)])
     return word == "MAS"
 
+def up_left(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if row < 3 or col < 3:
+        return False
+    word = list_to_single_str([grid[i][j] for i, j in zip(range(row-3, row), range(col-3, col))])
+    return word == "SAM"
+
+def up_right(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if row < 3 or len(grid[row]) -1 - col < 3:
+        return False
+    word = list_to_single_str([grid[i][j] for i, j in zip(range(row-3, row), reversed(range(col+1, col+4)))])
+    return word == "SAM"
+
+def down_right(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if len(grid)-1-row < 3 or len(grid[row]) -1 - col < 3:
+        return False
+    word = list_to_single_str([grid[i][j] for i, j in zip(range(row+1, row+4), range(col+1, col+4))])
+    return word == "MAS"
+    
+def down_left(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if len(grid)-1-row < 3 or col < 3:
+        return False
+    word = list_to_single_str([grid[i][j] for i, j in zip(range(row+1, row+4), reversed(range(col-3, col)))])
+    return word == "MAS"
+
+
 
 def count_xmas(grid: list[str]) -> int:
     # Strategy: find an 'X' and work from there in each
     # direction.
-    pass
+    ret = 0
+    for rowidx in range(len(grid)):
+        for colidx in range(len(grid[rowidx])):
+            if grid[rowidx][colidx] == 'X':
+                ret += sum([
+                    up(grid, rowidx, colidx),
+                    down(grid, rowidx, colidx),
+                    left(grid, rowidx, colidx),
+                    right(grid, rowidx, colidx),
+                    up_right(grid, rowidx, colidx),
+                    up_left(grid, rowidx, colidx),
+                    down_left(grid, rowidx, colidx),
+                    down_right(grid, rowidx, colidx),
+                ])
+    return ret
+
 
 
 if __name__ == "__main__":
--- test_day4.py
+++ test_day4.py
@@ -1,38 +1,57 @@
 import pytest
-from day4 import count_xmas, right, left, up, down
+from day4 import (
+    count_xmas,
+    right,
+    left,
+    up,
+    down,
+    up_left,
+    up_right,
+    down_left,
+    down_right,
+)
 
 
-def test_right() -> None:
-    example = ["XXXXX", "XXMAS"]
-    assert right(example, 1, 1)
-    assert not right(example, 0, 0)
-    assert not right(example, 0, 3)
-    assert not right(example, 1, 0)
+@pytest.fixture
+def example() -> list[str]:
+    return [
+        "MMMSXXMASM",  #  0
+        "MSAMXMSMSA",  # 1
+        "AMXSXMAAMM",  # 2
+        "MSAMASMSMX",  # 3
+        "XMASAMXAMM",  # 4
+        "XXAMMXXAMA",
+        "SMSMSASXSS",  # 6
+        "SAXAMASAAA",
+        "MAMMMXMMMM",  # 8
+        "MXMXAXMAS",
+    ]
 
 
-def test_left() -> None:
-    example = ["XXXXX", "SAMXX"]
-    assert left(example, 1, 3)
-    assert not left(example, 0, 0)
-    assert not left(example, 0, 3)
+def test_right(example: list[str]) -> None:
+    assert right(example, 4, 0)
+    assert right(example, 0, 5)
+    assert not right(example, 0, 4)
+    assert not right(example, 3, 9)
+
+
+def test_left(example: list[str]) -> None:
+    assert left(example, 1, 4)
+    assert not left(example, 0, 5)
     # Not an X:
     with pytest.raises(AssertionError):
         assert not left(example, 1, 0)
 
 
-def test_up() -> None:
-    example = ["XSXXX", "SAMXX", "XMXXA", "XXXXX"]
-    assert up(example, 3, 1)
-    assert not up(example, 0, 0)
-    assert not up(example, 0, 3)
-    assert not up(example, 3, 3)
+def test_up(example: list[str]) -> None:
+    assert up(example, 5, 6)
+    assert not up(example, 5, 0)
     # Not an X:
     with pytest.raises(AssertionError):
         assert not up(example, 1, 0)
 
 
-def test_down() -> None:
-    example = ["XXXXX", "XMXXA", "SAMXX", "XSXXX"]
+def test_down(example: list[str]) -> None:
     assert down(example, 0, 1)
     assert not down(example, 0, 0)
     assert not down(example, 0, 3)
@@ -42,17 +61,23 @@ def test_down() -> None:
         assert not down(example, 1, 1)
 
 
-def test_example() -> None:
-    example: list[str] = [
-        "MMMSXXMASM",
-        "MSAMXMSMSA",
-        "AMXSXMAAMM",
-        "MSAMASMSMX",
-        "XMASAMXAMM",
-        "XXAMMXXAMA",
-        "SMSMSASXSS",
-        "SAXAMASAAA",
-        "MAMMMXMMMM",
-        "MXMXAXMAS",
-    ]
+def test_up_left(example: list[str]) -> None:
+    assert up_left(example, 5, 6)
+    assert not up_left(example, 4, 6)
+
+
+def test_up_right(example: list[str]) -> None:
+    assert up_right(example, 9, 1)
+
+
+def test_down_right(example: list[str]) -> None:
+    assert down_right(example, 0, 4)
+    assert not down_right(example, 5, 6)
+
+
+def test_down_left(example: list[str]) -> None:
+    assert down_left(example, 3, 9)
+
+
+def test_example(example: list[str]) -> None:
     assert count_xmas(example) == 18
