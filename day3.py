b4b5e916ebfa2e987a5b01b86a49161289be7328
Lasse Hinrichsen-Bischoff <lh1887@mi.fu-berlin.de>
2024-12-04 09:28:40 +0100

    3 semi working

--- day3.py
+++ day3.py
@@ -17,6 +17,7 @@ def str_to_int_prod(m: str) -> int:
 
 
 def products(word: str) -> list[int]:
+    """Takes a list of strings like mul(xx,yy) and returns [xx*yy, ...]."""
     p = re.compile("mul\(\d{1,3},\d{1,3}\)")
     prod_strs = p.findall(word)
 
@@ -24,20 +25,52 @@ def products(word: str) -> list[int]:
 
 
 def do_dont_products(word: str) -> list[str]:
-    p = re.compile("mul\(\d{1,3},\d{1,3}\)|do\(\)|dont\(\)")
+    p = re.compile("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
     return p.findall(word)
 
 
+class EnableDisableMachine:
+    """Stateful callable which is either enabled or disabled.
+
+    If we found a don't(), we are disabled until the next do().
+    For anything which is not mul(...), we always return False, but may
+    change the state of the machine.
+
+    For a string like mul(...), we return True or False depending on the state
+    of the machine (i.e. if it is enabled or not).
+    """
+
+    def __init__(self):
+        self.enabled = True
+
+    def __call__(self, word: str) -> bool:
+        if word == "don't()":
+            self.enabled = False
+            return False
+        elif word == "do()":
+            self.enabled = True
+            return False
+        return self.enabled
+
+
 def filter_donts(words: list[str]) -> list[str]:
-    pass
+    enabled = EnableDisableMachine()
+    return [word for word in words if enabled(word)]
 
 
 def part_1(words: list[str]) -> int:
     return sum([sum(products(word)) for word in words])
+
+def list_to_single_str(words: list[str]) -> str:
+    return "".join(words)
+
+def part_2(words: list[str]) -> int:
+    filtered_words = [list_to_single_str(filter_donts(do_dont_products(word))) for word in words]
+    return sum([sum(products(word)) for word in filtered_words])
 
 
 if __name__ == "__main__":
     # print(parse_input("day3.txt"))
     input = parse_input("day3.txt")
     print(part_1(input))
-    # print(part_2(input))
+    print(part_2(input))
