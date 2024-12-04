7b15126a8f5107bde6a453a17186ad8e6feb3136
Lasse Hinrichsen-Bischoff <lh1887@mi.fu-berlin.de>
2024-12-04 10:08:25 +0100

    Day 4 wip

--- day4.py
+++ day4.py
@@ -1,8 +1,49 @@
+from day3 import list_to_single_str
+
+
 def parse_input(file_name) -> list[list[int]]:
     pass
 
+
+def right(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if len(grid[row]) - 1 - col < 3:
+        return False
+    return grid[row][col + 1 : col + 4] == "MAS"
+
+
+def left(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if col < 3:
+        return False
+    return grid[row][col - 3 : col] == "SAM"
+
+
+def up(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if row < 3:
+        return False
+    word = list_to_single_str([grid[i][col] for i in range(row - 3, row)])
+    return word == "SAM"
+
+
+def down(grid: list[str], row: int, col: int) -> bool:
+    assert grid[row][col] == "X"
+
+    if len(grid) - 1 - row < 3:
+        return False
+    word = list_to_single_str([grid[i][col] for i in range(row + 1, row + 4)])
+    return word == "MAS"
+
+
 def count_xmas(grid: list[str]) -> int:
+    # Strategy: find an 'X' and work from there in each
+    # direction.
     pass
+
 
 if __name__ == "__main__":
     print(parse_input("day4.txt"))
--- test_day4.py
+++ test_day4.py
@@ -1,4 +1,45 @@
-from day4 import count_xmas
+import pytest
+from day4 import count_xmas, right, left, up, down
+
+
+def test_right() -> None:
+    example = ["XXXXX", "XXMAS"]
+    assert right(example, 1, 1)
+    assert not right(example, 0, 0)
+    assert not right(example, 0, 3)
+    assert not right(example, 1, 0)
+
+
+def test_left() -> None:
+    example = ["XXXXX", "SAMXX"]
+    assert left(example, 1, 3)
+    assert not left(example, 0, 0)
+    assert not left(example, 0, 3)
+    # Not an X:
+    with pytest.raises(AssertionError):
+        assert not left(example, 1, 0)
+
+
+def test_up() -> None:
+    example = ["XSXXX", "SAMXX", "XMXXA", "XXXXX"]
+    assert up(example, 3, 1)
+    assert not up(example, 0, 0)
+    assert not up(example, 0, 3)
+    assert not up(example, 3, 3)
+    # Not an X:
+    with pytest.raises(AssertionError):
+        assert not up(example, 1, 0)
+
+
+def test_down() -> None:
+    example = ["XXXXX", "XMXXA", "SAMXX", "XSXXX"]
+    assert down(example, 0, 1)
+    assert not down(example, 0, 0)
+    assert not down(example, 0, 3)
+    assert not down(example, 3, 3)
+    # Not an X:
+    with pytest.raises(AssertionError):
+        assert not down(example, 1, 1)
 
 
 def test_example() -> None:
